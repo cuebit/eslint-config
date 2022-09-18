c78d060827ed30c50b349c69779a0f04fcf38efe
Cue <1493221+cuebit@users.noreply.github.com>
2022-09-18 05:16:27 +0100

    feat: enforce `curly` for all control statements

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: '@antfu',
   rules: {
+    'curly': ['error', 'all'],
     'import/order': ['error', { pathGroups: [{ pattern: '@/**', group: 'external' }] }],
-    'new-cap': ['error', { newIsCap: false, capIsNew: false, properties: true }
-    ],
+    'new-cap': ['error', { newIsCap: false, capIsNew: false, properties: true }],
     // Typescript
     '@typescript-eslint/comma-dangle': ['error', 'never'],
     // Vue

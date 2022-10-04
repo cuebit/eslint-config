9c5a6e49a5fdb61c12789f0ededcb681ddd36ecf
Cue <1493221+cuebit@users.noreply.github.com>
2022-10-04 13:31:11 +0100

    feat: common import ordering

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ module.exports = {
   extends: '@antfu',
   rules: {
     'curly': ['error', 'all'],
-    'import/order': ['error', { pathGroups: [{ pattern: '@/**', group: 'external' }] }],
+    'import/order': ['error', {
+      alphabetize: { order: 'asc', caseInsensitive: false },
+      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      pathGroups: [
+        {
+          pattern: '@/composables/**',
+          group: 'parent',
+          position: 'before'
+        },
+        {
+          pattern: '@/components/**',
+          group: 'parent',
+          position: 'before'
+        },
+        {
+          pattern: '@/**',
+          group: 'internal',
+          position: 'after'
+        }
+      ]
+    }],
     'new-cap': ['error', { newIsCap: false, capIsNew: false, properties: true }],
+
     // Typescript
     '@typescript-eslint/comma-dangle': ['error', 'never'],
+
     // Vue
     'vue/attributes-order': 'off',
     'vue/html-closing-bracket-newline': 'off',
     'vue/singleline-html-element-content-newline': 'off'
   }
 }
-
